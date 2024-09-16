894116a5978ef980737a5ec58edf588dced44876
Kabby <kabby.dev@outlook.com>
2024-09-16 18:04:14 +0100

    sizes

--- components/footer.tsx
+++ components/footer.tsx
@@ -56,8 +56,8 @@ export default function Footer() {
           </Link>
           <Link href="" className="bg-gray-100 p-1 rounded-full bg-opacity-50">
             <svg
-              width="24"
-              height="24"
+              width="20"
+              height="20"
               viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
