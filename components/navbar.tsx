01773f558c2b2df12528eca5f7b008cdd1353597
Kabby <kabby.dev@outlook.com>
2024-09-16 12:46:19 +0100

    sidebar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,15 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 
 export default function NavBar() {
+  const [openSideBar, setOpenSideBar] = useState(false);
   return (
-    <nav className="h-[15vh] w-[100vw] border-b">
-      <div className="w-full h-1/6 primary"></div>
-      <div className="flex items-center justify-center w-full h-5/6">
-        <div className="flex justify-between w-full px-12">
+    <nav className="md:h-[15vh] h-[10vh] w-[100vw] border-b">
+      <div className="w-full h-1/6 primary md:block hidden"></div>
+      <div className="flex items-center justify-center w-full md:h-5/6 h-full">
+        <div className="flex justify-between w-full md:px-12 px-4">
           <div className="flex items-center gap-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -19,12 +23,78 @@ export default function NavBar() {
                 fill="#2C2B2B"
               />
             </svg>
-            <p className="">Receitas</p>
+            <p className="md:block hidden">Receitas</p>
           </div>
-          <div className="flex items-center gap-12 font-semibold text-[#2C2B2B]">
-            <Link href="/">Home</Link>
+          <div
+            className={`flex md:flex-row flex-col md:items-center gap-12 md:font-semibold text-[#2C2B2B] md:relative absolute primary md:bg-white top-0 h-[100vh] w-[80vw] md:w-fit md:h-fit px-8 py-16 md:p-0 text-[16px] transition-all duration-500 ${
+              openSideBar
+                ? "translate-x-[-5vw] md:translate-x-0"
+                : "translate-x-[-100vw] md:translate-x-0"
+            }`}
+          >
+            <Link href="/" className="flex items-center gap-2">
+              <svg
+                width="28px"
+                height="26px"
+                viewBox="0 0 24.00 24.00"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="md:hidden block"
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <path
+                    d="M9 16C9.85038 16.6303 10.8846 17 12 17C13.1154 17 14.1496 16.6303 15 16"
+                    stroke="#000000"
+                    stroke-width="2.4"
+                    stroke-linecap="round"
+                  ></path>{" "}
+                  <path
+                    d="M22 12.2039V13.725C22 17.6258 22 19.5763 20.8284 20.7881C19.6569 22 17.7712 22 14 22H10C6.22876 22 4.34315 22 3.17157 20.7881C2 19.5763 2 17.6258 2 13.725V12.2039C2 9.91549 2 8.77128 2.5192 7.82274C3.0384 6.87421 3.98695 6.28551 5.88403 5.10813L7.88403 3.86687C9.88939 2.62229 10.8921 2 12 2C13.1079 2 14.1106 2.62229 16.116 3.86687L18.116 5.10812C20.0131 6.28551 20.9616 6.87421 21.4808 7.82274"
+                    stroke="#000000"
+                    stroke-width="2.4"
+                    stroke-linecap="round"
+                  ></path>{" "}
+                </g>
+              </svg>
+              Home
+            </Link>
 
-            <Link href="/recipes">Recipes</Link>
+            <Link href="/recipes" className="flex gap-2 items-center">
+              <svg
+                className="md:hidden block"
+                fill="#000000"
+                width="28px"
+                height="26px"
+                viewBox="0 0 122.88 122.88"
+                version="1.1"
+                id="Layer_1"
+                xmlns="http://www.w3.org/2000/svg"
+                stroke="#000000"
+                stroke-width="2.9491199999999997"
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <g>
+                    {" "}
+                    <path d="M97.34,0.74c0.86-0.93,2.3-0.99,3.23-0.13c0.93,0.86,0.99,2.3,0.13,3.23L81.98,24.1l-0.03,0.04 c-2.29,2.77-3.86,5.33-4.56,7.67c-0.62,2.07-0.53,3.95,0.39,5.59c0.49,0.88,0.33,1.96-0.32,2.67l0,0l-8.89,9.62 c-0.87-0.95-1.56-1.72-2.02-2.22c-0.21-0.28-0.45-0.55-0.7-0.81l-0.02,0.02c-0.12-0.13-0.25-0.25-0.38-0.37l7.6-8.23 c-0.89-2.38-0.88-4.91-0.06-7.6c0.88-2.92,2.75-6.03,5.44-9.27c0.06-0.08,0.11-0.16,0.18-0.23L97.32,0.72L97.34,0.74L97.34,0.74z M57.13,55.01c-0.84-0.94-0.76-2.39,0.18-3.23c0.94-0.84,2.39-0.76,3.23,0.18c9.41,10.54,38.5,41.73,46.56,53.39 c10.63,15.05-5.83,19.79-11.29,14.31c-13.64-13.19-42.6-46.82-55.33-61.08c-4.58,1.94-9.03,2.24-13.5,0.96 c-4.81-1.37-9.52-4.58-14.3-9.51l-0.06-0.06c-3.64-3.84-6.49-7.63-8.55-11.38c-2.11-3.86-3.4-7.68-3.86-11.47 c-0.49-4.08-0.11-7.88,0.99-11.25c1.29-3.96,3.58-7.31,6.58-9.8c3.02-2.5,6.73-4.12,10.87-4.62c3.44-0.41,7.19-0.06,11.07,1.21 c5.37,1.75,11.63,6.1,16.82,11.68c3.83,4.11,7.11,8.92,9.06,13.87c2.03,5.16,2.65,10.5,1.02,15.5c-0.96,2.96-2.7,5.74-5.4,8.25 c-0.93,0.86-2.37,0.8-3.23-0.12c-0.86-0.93-0.8-2.37,0.12-3.23c2.09-1.95,3.43-4.08,4.16-6.33c1.26-3.87,0.73-8.16-0.93-12.38 c-1.74-4.42-4.69-8.74-8.15-12.45c-4.68-5.02-10.23-8.91-14.91-10.44c-3.21-1.04-6.28-1.34-9.09-1c-3.26,0.4-6.18,1.65-8.51,3.6 c-2.34,1.95-4.13,4.58-5.16,7.71c-0.89,2.73-1.2,5.87-0.79,9.26c0.39,3.2,1.5,6.47,3.32,9.81c1.91,3.43,4.53,6.9,7.9,10.45 l0.02,0.03c4.22,4.35,8.27,7.15,12.28,8.29c3.79,1.08,7.65,0.66,11.68-1.35c0.92-0.53,2.11-0.35,2.84,0.47 c12.42,13.91,42.63,48.92,56.01,61.89c5.81,2.37,9.03-0.55,6.25-5.7C100.7,102.43,63.5,62.17,57.13,55.01L57.13,55.01L57.13,55.01z M45.07,75.12l-29.16,31.55c-0.06,0.06-0.11,0.12-0.18,0.18c-4.26,4.6,3.28,11.3,7.96,6.82l28.32-30.65l3.04,3.45l-28.1,30.41l0,0 c-0.06,0.07-0.12,0.13-0.2,0.2c-1.68,1.41-3.37,2.33-5.08,2.71c-1.76,0.4-3.49,0.22-5.15-0.56c-0.28-0.11-0.54-0.25-0.77-0.46 l-4.03-3.73l0,0c-0.06-0.06-0.12-0.11-0.18-0.18c-1.56-1.8-2.3-3.72-2.1-5.75c0.19-1.92,1.21-3.79,3.14-5.59l29.44-31.86 L45.07,75.12L45.07,75.12z M75.63,57.46l1.73-1.87c0.86-0.93,2.31-0.99,3.23-0.13s0.99,2.3,0.13,3.23l-2,2.16L75.63,57.46 L75.63,57.46z M104.45,7.43c0.86-0.93,2.3-0.99,3.23-0.13c0.93,0.86,0.99,2.3,0.13,3.23L91.4,28.3c-0.86,0.93-2.3,0.99-3.23,0.13 c-0.93-0.86-0.99-2.3-0.13-3.23L104.45,7.43L104.45,7.43L104.45,7.43z M111.55,14c0.86-0.93,2.3-0.99,3.23-0.13 c0.93,0.86,0.99,2.3,0.13,3.23L98.51,34.86c-0.86,0.93-2.3,0.99-3.23,0.13c-0.93-0.86-0.99-2.3-0.13-3.23L111.55,14L111.55,14 L111.55,14z M118.91,20.83c0.86-0.93,2.3-0.99,3.23-0.13c0.93,0.86,0.99,2.31,0.13,3.23L103.55,44.2c-0.07,0.07-0.14,0.13-0.21,0.2 c-4.26,4.1-8.33,6.47-12.22,7.14c-4.22,0.73-8.09-0.47-11.64-3.57c-0.95-0.83-1.04-2.28-0.22-3.22c0.83-0.95,2.28-1.04,3.22-0.22 c2.45,2.14,5.07,2.98,7.84,2.49c2.98-0.51,6.26-2.48,9.84-5.93l0.02-0.02l18.71-20.25L118.91,20.83L118.91,20.83z"></path>{" "}
+                  </g>{" "}
+                </g>
+              </svg>
+              Recipes
+            </Link>
             <Link href="/personalised-recipes" className="flex gap-1">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -117,9 +187,51 @@ export default function NavBar() {
                   stroke-width="2"
                 />
               </svg>
-              <p>Personalised Recipes</p>
+              <p>Personalised</p>
             </Link>
-            <Link href="/about">About</Link>
+            <Link href="/about" className="flex gap-2 items-center">
+              <svg
+                width="28px"
+                height="26px"
+                viewBox="0 0 512.00 512.00"
+                version="1.1"
+                fill="#000000"
+                className="md:hidden block"
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <title>about</title>{" "}
+                  <g
+                    id="Page-1"
+                    stroke-width="0.00512"
+                    fill="none"
+                    fill-rule="evenodd"
+                  >
+                    {" "}
+                    <g
+                      id="about-white"
+                      fill="#000000"
+                      transform="translate(42.666667, 42.666667)"
+                    >
+                      {" "}
+                      <path
+                        d="M213.333333,3.55271368e-14 C95.51296,3.55271368e-14 3.55271368e-14,95.51168 3.55271368e-14,213.333333 C3.55271368e-14,331.153707 95.51296,426.666667 213.333333,426.666667 C331.154987,426.666667 426.666667,331.153707 426.666667,213.333333 C426.666667,95.51168 331.154987,3.55271368e-14 213.333333,3.55271368e-14 Z M213.333333,384 C119.227947,384 42.6666667,307.43872 42.6666667,213.333333 C42.6666667,119.227947 119.227947,42.6666667 213.333333,42.6666667 C307.44,42.6666667 384,119.227947 384,213.333333 C384,307.43872 307.44,384 213.333333,384 Z M240.04672,128 C240.04672,143.46752 228.785067,154.666667 213.55008,154.666667 C197.698773,154.666667 186.713387,143.46752 186.713387,127.704107 C186.713387,112.5536 197.99616,101.333333 213.55008,101.333333 C228.785067,101.333333 240.04672,112.5536 240.04672,128 Z M192.04672,192 L234.713387,192 L234.713387,320 L192.04672,320 L192.04672,192 Z"
+                        id="Shape"
+                      >
+                        {" "}
+                      </path>{" "}
+                    </g>{" "}
+                  </g>{" "}
+                </g>
+              </svg>
+              About
+            </Link>
           </div>
           <div className="flex items-center gap-2  rounded-full bg-[#F5F2F2] pl-4">
             <svg
@@ -139,6 +251,72 @@ export default function NavBar() {
               placeholder="Search Recipes"
               className="focus:outline-none bg-[#F5F2F2] rounded-full p-1 placeholder:text-[#858585] placeholder:font-light placeholder:text-[15px]"
             />
+          </div>
+          <div className="md:hidden block">
+            {openSideBar ? (
+              <svg
+                onClick={() => setOpenSideBar(false)}
+                width="28px"
+                height="26px"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <g stroke="#000000" stroke-linecap="round" stroke-width="2">
+                    {" "}
+                    <path d="M6 18L18 6"></path> <path d="M18 18L6 6"></path>{" "}
+                  </g>{" "}
+                </g>
+              </svg>
+            ) : (
+              <svg
+                onClick={() => setOpenSideBar(true)}
+                width="28px"
+                height="26px"
+                viewBox="-0.5 0 25 25"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <path
+                    d="M2 12.32H22"
+                    stroke="#000000"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>{" "}
+                  <path
+                    d="M2 18.32H22"
+                    stroke="#000000"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>{" "}
+                  <path
+                    d="M2 6.32001H22"
+                    stroke="#000000"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>{" "}
+                </g>
+              </svg>
+            )}
           </div>
         </div>
       </div>
