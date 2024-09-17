224794338bb4f5312b1603f92ea0830374bdb719
Kabby <kabby.dev@outlook.com>
2024-09-17 01:02:56 +0100

    fix

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -1,4 +1,13 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 export default function Hero() {
+  const variants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+  };
+
   return (
     <header
       className="relative w-full md:h-[362px] h-[200px] flex items-center justify-center"
@@ -8,11 +17,17 @@ export default function Hero() {
         backgroundSize: "cover",
       }}
     >
-      <div className="bg-black opacity-30 absolute w-full h-full z-1"></div>
-      <h1 className="absolute w-full px-8 md:px-16 text-center text-[#fff] md:text-[40px] text-[20px] font-bold z-10">
+      <div className="bg-black opacity-30 absolute w-full h-full z-10"></div>
+      <motion.div
+        className="absolute w-full px-8 md:px-16 text-center text-[#fff] md:text-[40px] text-[20px] font-bold z-20"
+        initial="hidden"
+        animate="visible"
+        variants={variants}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+      >
         Be inspired, cook with passion, and savor unforgettable moments at the
         table.
-      </h1>
+      </motion.div>
     </header>
   );
 }
