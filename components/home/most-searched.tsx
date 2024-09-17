d13b32d9015d55935a62362cca99a8d33ffb7182
Kabby <kabby.dev@outlook.com>
2024-09-17 01:26:04 +0100

    fix

--- components/home/most-searched.tsx
+++ components/home/most-searched.tsx
@@ -1,7 +1,9 @@
 export default function MostSearched() {
   return (
-    <div className="w-full h-[60vh] bg-gray-100">
-      <h2 className="text-center">Most Searched Recipes</h2>
+    <div className="w-full h-[60vh] bg-gray-100 md:p-16 p-8">
+      <h2 className="text-center font-bold md:text-[30px] text-[18px] text-[#2C2B2B]">
+        Most Searched Recipes
+      </h2>
     </div>
   );
 }
