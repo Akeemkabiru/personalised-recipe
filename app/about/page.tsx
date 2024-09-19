99c3979d4d181b710f7bb7fa3dcc5f189b8ed904
Kabby <kabby.dev@outlook.com>
2024-09-19 09:05:17 +0100

    fix

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,42 @@
 export default function Page() {
-  return <div>About</div>;
+  return (
+    <div className="w-full md:px-36 md:py-8 p-4 font-normal md:space-y-4 space-y-2 md:text-[14px] text-[16px]">
+      <div>
+        Hello! We are Amanda and Pedro, and we’re excited to present our
+        project, Delícias à Mesa. As we explored this incredible site, we
+        discovered a gastronomic universe filled with delicious recipes, helpful
+        tips, and culinary inspiration. Delícias à Mesa is a place where
+        seasoned chefs and beginner cooks can unite in their shared passion for
+        food.
+      </div>
+      <div>
+        One of the things that truly captivated us is the variety of recipes
+        available. From traditional comfort foods to innovative creations,
+        there’s something for every taste and occasion. Each recipe is carefully
+        selected and tested, ensuring that the results are always flavorful and
+        worth sharing.
+      </div>
+      <div>
+        In addition to the recipes, Delícias à Mesa offers practical tips to
+        improve your cooking skills. From preparation techniques to
+        flavor-pairing suggestions, the site encourages you to explore and
+        experiment in the kitchen. It is a welcoming and inclusive environment
+        where everyone is invited to dive into the art of cooking and discover
+        new possibilities.
+      </div>
+      <div>
+        What makes Delícias à Mesa even more exciting is that it’s powered by
+        AI. This means the app can suggest personalized recipes based on your
+        preferences and the ingredients you have on hand. Whether you are
+        looking for inspiration or need help with meal planning, the AI helps
+        make cooking easier and more fun.
+      </div>
+      <div>
+        In short, Delícias à Mesa is an inspiring culinary space that invites
+        you to explore, create, and share your love for food. We hope our
+        presentation has sparked your interest in joining us on this delicious
+        journey with Delícias à Mesa!
+      </div>
+    </div>
+  );
 }
